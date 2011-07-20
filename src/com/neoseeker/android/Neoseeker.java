811fd402fff01234fa8f40e072f246d7cb2a4c11
Chris Higgins <chris@chigs.me>
2011-07-20 09:15:57 -0700

    Edited src/com/neoseeker/android/Neoseeker.java via GitHub

--- src/com/neoseeker/android/Neoseeker.java
+++ src/com/neoseeker/android/Neoseeker.java
@@ -5,8 +5,8 @@ import android.app.Application;
 public class Neoseeker extends Application {
 	public static final String PREFS_NAME = "NeoseekerPrefs";
 	
-	private static String consumerKey 			= "48f9f74808404b41117c2a47368c4a";
-	private static String consumerSecret 		= "a4680bc19b";
+	private static String consumerKey 			= "";
+	private static String consumerSecret 		= "";
 	
 	private static NeoAPI neoAPI = new NeoAPI(Neoseeker.consumerKey, Neoseeker.consumerSecret);
 	
