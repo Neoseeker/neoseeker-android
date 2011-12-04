80144576857c4bd370c54ce476b2eb4186ae385c
Chris Higgins <chris@chigs.me>
2011-12-03 22:30:25 -0600

    Keep on forgetting to take our consumer tokens :|

--- src/com/neoseeker/android/Neoseeker.java
+++ src/com/neoseeker/android/Neoseeker.java
@@ -5,8 +5,8 @@ import android.app.Application;
 public class Neoseeker extends Application {
 	public static final String PREFS_NAME = "NeoseekerPrefs";
 	
-	private static String consumerKey 			= "c260b1ca7cba2527157b2acee4c85d";
-	private static String consumerSecret 		= "3dc2305ffd";
+	private static String consumerKey 			= "";
+	private static String consumerSecret 		= "";
 	
 	private static NeoAPI neoAPI = new NeoAPI(Neoseeker.consumerKey, Neoseeker.consumerSecret);
 	
